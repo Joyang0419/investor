db1133e6b964f29bb486ea57ee0f741a14da713c
eric_liu <eric_liu@paradise-soft.com.tw>
2024-03-23 10:42:29 +0800

    feat(graphql): todo example

--- apiserver/service/example.go
+++ apiserver/service/example.go
@@ -4,13 +4,12 @@ import (
 	"apiserver/handler"
 )
 
-// todo 這邊還是需要service 層 因為之後是GraphQL, 要在這邊組裝response
-
-type ExampleService struct {
-}
-
 // 故意的，compile時, 如果沒有implement IExampleService, 會直接不能compile
 var _ handler.IExampleService = NewExampleService()
+
+// todo 這邊還是需要service 層 因為之後是GraphQL, 要在這邊組裝response
+type ExampleService struct {
+}
 
 func NewExampleService() *ExampleService {
 	return &ExampleService{}

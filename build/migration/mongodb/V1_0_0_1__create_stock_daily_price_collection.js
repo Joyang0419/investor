c8a24c198914ada36e8af72b6653c9aedaaa1187
Joy Yang <ptp_joy@photons.com.tw>
2024-04-27 11:25:28 +0800

    feat(): implement mongodb migration
    # Why
    - 版本控制mongodb
    
    # How
    - 選用migrate-mongo

---
+++ build/migration/mongodb/V1_0_0_1__create_stock_daily_price_collection.js
@@ -0,0 +1,17 @@
+module.exports = {
+    async up(db, client) {
+        // 從 client 對象中獲取 'investor' 數據庫實例
+        const db_investor = client.db("investor");
+
+        // 在 'investor' 數據庫中創建一個新的集合 'stock_daily_price'
+        await db_investor.createCollection('stock_daily_price');
+    },
+
+    async down(db, client) {
+        // 從 client 對象中獲取 'investor' 數據庫實例
+        const db_investor = client.db("investor");
+
+        // 在 'investor' 數據庫中創建一個新的集合 'stock_daily_price'
+        await db_investor.collection('stock_daily_price').drop();
+    }
+};

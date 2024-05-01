14e5532c398d6768489f890c5d943bffa55fae8d
eric_liu <eric_liu@paradise-soft.com.tw>
2024-05-01 12:24:03 +0800

    feat(KAN-2): viper read env.yaml setting

--- apiserver/cmd/server.go
+++ apiserver/cmd/server.go
@@ -3,15 +3,15 @@ package cmd
 import (
 	"log"
 
-	"github.com/gin-gonic/gin"
-
-	"github.com/spf13/cobra"
-
 	graphql "apiserver/graphql/resolver"
 	"apiserver/middleware"
 	"apiserver/router"
 	"tools/encryption"
 	"tools/logger"
+
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var serverCmd = &cobra.Command{
@@ -21,18 +21,36 @@ var serverCmd = &cobra.Command{
 	Run:   runServerCmd,
 }
 
-// TODO viper to get secret and port
-var secret = []byte("ejkorjqwiejriwejri")
-var port = ":8080"
-
 func init() {
 	rootCmd.AddCommand(serverCmd)
+
+	// viper get config from env.yaml
+	viper.SetConfigName("env")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	// load env.yaml by viper
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("error reading config file, %s", err)
+	}
+
+	// default value
+	{
+		viper.SetDefault("server.port", ":8080")
+
+		viper.SetDefault("jwt.secret", []byte(`kmkdmvqejmriqiwngijoqpw`))
+
+		viper.SetDefault("oauth2.google.client_id", "client_id")
+		viper.SetDefault("oauth2.google.client_secret", "client_secret")
+		viper.SetDefault("oauth2.google.redirect_url", "http://localhost:8080/auth/google/callback")
+		viper.SetDefault("oauth2.google.scopes", []string{})
+	}
 }
 
 // TODO graceful shutdown: https://learnku.com/docs/gin-gonic/1.5/examples-graceful-restart-or-stop/6173
 func runServerCmd(_ *cobra.Command, _ []string) {
 	jwtEncryption := encryption.NewJWTEncryption[middleware.TokenInfo](encryption.JWTRequirements{
-		SecretKey:     secret,
+		SecretKey:     []byte(viper.GetString("jwt.secret")),
 		SigningMethod: encryption.JWTSigningMethodHS256,
 	})
 
@@ -45,7 +63,7 @@ func runServerCmd(_ *cobra.Command, _ []string) {
 		jwtEncryption,
 	)
 
-	if err := r.Run(port); err != nil {
+	if err := r.Run(viper.GetString("server.port")); err != nil {
 		log.Fatalf("[runServerCmd]r.Run err: %v", err)
 	}
 

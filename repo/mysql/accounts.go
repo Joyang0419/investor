a79e4c7be919df8a7965a6531b356fc08b41c90e
eric_liu <eric_liu@paradise-soft.com.tw>
2024-05-31 10:39:27 +0800

    chore: adjust account name

--- repo/mysql/accounts.go
+++ repo/mysql/accounts.go
@@ -7,7 +7,7 @@ import (
 type Account struct {
 	ID          int       `gorm:"column:id"`            // 帳戶ID
 	Email       string    `gorm:"column:email"`         // 電子郵件
-	AccountName string    `gorm:"column:account_name"`  // 帳戶名稱
+	Name        string    `gorm:"column:name"`          // 帳戶名稱
 	Picture     string    `gorm:"column:picture"`       // 大頭貼URL
 	Balance     float64   `gorm:"column:balance"`       // 餘額
 	LastLoginAt time.Time `gorm:"column:last_login_at"` // 最後登入時間

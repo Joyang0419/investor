9ddd2a6a018d57539a493d21b87f98a1dc783cb7
eric_liu <eric_liu@paradise-soft.com.tw>
2024-06-03 10:03:56 +0800

    fix: mysql migration table entity

--- build/migration/mysql/V1_0_0_4__create_transactions.sql
+++ build/migration/mysql/V1_0_0_4__create_transactions.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS transactions
 (
-    transaction_id INT AUTO_INCREMENT PRIMARY KEY COMMENT '交易ID',
+    id INT AUTO_INCREMENT PRIMARY KEY COMMENT '交易ID',
     type ENUM('deposit', 'withdrawal', 'transfer') NOT NULL COMMENT '交易类型',
     amount DECIMAL(10, 2) NOT NULL COMMENT '交易金额',
     account_id INT NOT NULL COMMENT '交易账户ID',
